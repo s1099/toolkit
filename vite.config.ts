d6d824600e48ead95435487e9dfd0b2b6d5de138
s1099 <46890315+s1099@users.noreply.github.com>
2025-07-27 00:00:39 +0530

    gh pages workflow, update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
+  base: "/toolkit/",
 });
