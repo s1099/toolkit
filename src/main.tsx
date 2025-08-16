01da560e333750f9f1bf6c8fe68357f3bdb28ce3
s1099 <46890315+s1099@users.noreply.github.com>
2025-08-17 00:55:35 +0530

    bump deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import { StrictMode } from "react";
 import { AppRouter } from "./router";
 import "./index.css";
 
