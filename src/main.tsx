cbbaf4a4e14a074ed69ad8f98bc68471074b6438
s1099 <46890315+s1099@users.noreply.github.com>
2025-09-22 19:04:41 +0530

    i love Hash browns

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import { AppRouter } from "./router";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <AppRouter />
-    </BrowserRouter>
+  <HashRouter basename={import.meta.env.BASE_URL}>
+    <AppRouter />
+  </HashRouter>
 );
